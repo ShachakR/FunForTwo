9f5d79d26cb0f791378d02d34124a454b48bce0a
ShachakR <59149573+ShachakR@users.noreply.github.com>
2021-09-17 14:19:57 -0400

    First commit
    setup and initial testing

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const app = express(); // not using new puts this on the global window
+const port = 3000; 
+
+ 
+app.get('/', function (req, res) {
+    try{
+        res.sendFile(path.join(__dirname,'frontend/home.html'));
+    }catch(err){
+        res.status(404);
+        res.send('Error 404: failed to load page');
+    }
+    
+  })
+   
+  app.listen(port, () => {
+    console.log(`server started`);
+  })
 
