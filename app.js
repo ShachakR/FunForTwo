c72108a0fe49741e184e35d3551f7915d930b9b2
ShachakR <59149573+ShachakR@users.noreply.github.com>
2021-09-17 15:08:03 -0400

    Home Page update
    set up initial homepage look -> temporary

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
-const app = express(); // not using new puts this on the global window
+const app = express(); // not using keyword 'new' puts this on the global window
 const port = 3000; 
 
- 
+app.use(express.static("frontend"));
+
 app.get('/', function (req, res) {
     try{
-        res.sendFile(path.join(__dirname,'frontend/home.html'));
+        res.sendFile(path.join(__dirname,'frontend/home/home.html'));
     }catch(err){
         res.status(404);
         res.send('Error 404: failed to load page');
