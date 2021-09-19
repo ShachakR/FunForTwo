cdefdfcb550fcaf57be6ba2ac2c41cd8b9a93333
ShachakR <59149573+ShachakR@users.noreply.github.com>
2021-09-18 23:37:05 -0400

    Format changes

--- frontend/games/connectFour/main.js
+++ frontend/games/connectFour/main.js
@@ -1,15 +1,15 @@
 var client = io();
 const gameType = "connectFour";
 
-window.onload = function(){ 
+window.onload = function() {
     const gameCodeLabel = document.getElementById('gameCode');
     const playerCountLabel = document.getElementById('playerCount');
-    const location  = window.location.href.split('=')[1];
+    const location = window.location.href.split('=')[1];
 
-    client.emit('changedPage', { 'gameType' : gameType, 'location' : location}); // they either join a server or create one
+    client.emit('changedPage', { 'gameType': gameType, 'location': location }); // they either join a server or create one
 
     client.on('joined', (data) => {
         playerCountLabel.innerHTML = data.currentPlayers;
-        gameCodeLabel.innerHTML = `Game Code = ${data.gameId}`; 
+        gameCodeLabel.innerHTML = `Game Code = ${data.gameId}`;
     });
 }
