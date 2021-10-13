977a38a387233d06879a1363076b0c820482e50a
ShachakR <59149573+ShachakR@users.noreply.github.com>
2021-10-13 01:23:25 -0400

    UI changes

--- frontend/games/battleShip/main.js
+++ frontend/games/battleShip/main.js
@@ -1,7 +1,24 @@
 const gameType = "battleShip";
+const WDITH = 10;
 
 loadGame();
 
 function loadGame() {
 
+}
+
+function createElements() {
+    const gameScreen = document.getElementById("gameScreen-Container");
+    const canvas = document.createElement("div");
+    canvas.id = "canvas";
+
+    for (let r = 0; r < WDITH; r++) {
+        for (let c = 0; c < WDITH; c++) {
+            const cell = document.createElement("div");
+            cell.className = "cell";
+            cell.id = `row-${r} col-${c}`;
+            canvas.appendChild(cell);
+        }
+    }
+    gameScreen.appendChild(canvas);
 }
